a16271896dbbdcb38848d029322c425cab1ff0ce
horothesun <horothesun@gmail.com>
2024-01-12 19:51:48 +0000

    Xmonad: startup hook

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -11,6 +11,10 @@ import XMonad.Hooks.EwmhDesktops
 
 import XMonad.Hooks.DynamicLog
 
+import XMonad.Util.SpawnOnce
+
+myTerminal = "alacritty"
+
 myLayout = tiled ||| Mirror tiled ||| Full
   where
     tiled   = Tall nmaster delta ratio
@@ -22,12 +26,17 @@ myConfig = def
     { modMask    = mod4Mask  -- Rebind Mod to the Super key
     , layoutHook = myLayout  -- Use custom layouts
     , handleEventHook = handleEventHook def <+> fullscreenEventHook
+    , startupHook = myStartupHook
     }
   `additionalKeysP`
     [ ("M-S-z", spawn "xscreensaver-command -lock")
     , ("M-C-s", unGrab *> spawn "scrot -s"        )
     , ("M-f"  , spawn "firefox"                   )
     ]
+
+myStartupHook :: X ()
+myStartupHook = do
+    spawnOnce "xrandr --output \"eDP-1\" --mode \"1920x1200\" --rate \"60\" --scale \"1x1\""
 
 main :: IO ()
 main = xmonad . ewmh =<< xmobar myConfig
