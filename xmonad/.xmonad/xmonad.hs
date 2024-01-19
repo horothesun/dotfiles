b366408a65aadb2d2a78bf08f54ee10c5aebd72b
horothesun <horothesun@gmail.com>
2024-01-19 19:22:57 +0000

    Xmonad: nm-applet

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -28,6 +28,7 @@ myLayout = tiled ||| Mirror tiled ||| Full
 myStartupHook :: X ()
 myStartupHook = do
   spawnOnce internalMonitorSetResolutionCommand
+  spawnOnce "nm-applet &"
   spawnOnce "volumeicon &"
   spawnOnce "trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x282c34 --height 22 &"
   spawnOnce setKeyboardRepeatDelayAndRateCommand
