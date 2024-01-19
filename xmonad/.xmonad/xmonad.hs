fddcb4b79177fb68cc75a695503c96f7cb3c3539
horothesun <horothesun@gmail.com>
2024-01-19 22:23:16 +0000

    sxhkd: first test

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -31,6 +31,7 @@ myStartupHook = do
   spawnOnce "nm-applet &"
   spawnOnce "volumeicon &"
   spawnOnce "trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x282c34 --height 22 &"
+  spawnOnce "sxhkd &"
   spawnOnce setKeyboardRepeatDelayAndRateCommand
 
 myManageHook :: ManageHook
