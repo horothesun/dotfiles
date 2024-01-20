284ade5fd7753a5fef30c6107cc66f0a5a1b9cf5
horothesun <horothesun@gmail.com>
2024-01-20 16:06:00 +0000

    Xmonad: CopyQ

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -33,6 +33,7 @@ myStartupHook = do
   spawnOnce "trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x282c34 --height 22 &"
   -- spawnOnce "sxhkd &"
   spawnOnce setKeyboardRepeatDelayAndRateCommand
+  spawnOnce "copyq &"
 
 myManageHook :: ManageHook
 myManageHook = composeAll
