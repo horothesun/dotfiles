cf65259d211990e26e745bbdaf110fd12eae6c5e
horothesun <horothesun@gmail.com>
2024-01-21 15:53:11 +0000

    Xmonad: cleanup

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -31,10 +31,10 @@ myStartupHook = do
   spawnOnce internalMonitorSetResolutionCommand
   spawnOnce "nm-applet &"
   spawnOnce "volumeicon &"
+  spawnOnce "copyq &"
   spawnOnce "trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x282c34 --height 22 --iconspacing 4 &"
   -- spawnOnce "sxhkd &"
   spawnOnce setKeyboardRepeatDelayAndRateCommand
-  spawnOnce "copyq &"
 
 myManageHook :: ManageHook
 myManageHook = composeAll
