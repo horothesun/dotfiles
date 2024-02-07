5ffe62230163c7daa14d9cac482216db4dcd8191
horothesun <horothesun@gmail.com>
2024-02-07 00:08:48 +0000

    Xmonad: screen layout hi-res script

--- xmonad/bin/xinternal_only.sh
+++ xmonad/bin/xinternal_only.sh
@@ -4,7 +4,7 @@ INTERNAL_DISPLAY_NAME="eDP1"
 HDMI_DISPLAY_NAME="HDMI2"
 
 xrandr \
-  --output "${INTERNAL_DISPLAY_NAME}" --primary --mode "1920x1200" --rate "60" --scale "1x1" --pos "320x1440" --rotate "normal" \
+  --output "${INTERNAL_DISPLAY_NAME}" --primary --mode "1920x1200" --rate "60" --scale "1x1" --pos "0x0" --rotate "normal" \
   --output "${HDMI_DISPLAY_NAME}" --off \
   --output "DP1" --off \
   --output "DP2" --off \
