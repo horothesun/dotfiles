d4f3b488d831cf290b47ab1c02580d040a1145ff
horothesun <horothesun@gmail.com>
2024-03-02 18:40:09 +0000

    Xmobar: auto-cpufreq governor

---
+++ xmobar/bin/auto-cpufreq-governor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+( timeout 3s auto-cpufreq --stats | grep --line-buffered --max-count=1 "governor" ) 2>/dev/null | \
+  xargs -I ^ echo "\"^\"" | \
+  jq --raw-output '
+      split(" ")[-2]
+    | if (. == "powersave") then "󱙷 "
+      else
+        if (. == "performance") then "󱐋 " else "???" end
+      end
+  '
