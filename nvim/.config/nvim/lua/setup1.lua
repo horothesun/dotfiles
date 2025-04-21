6d0f68de39e55ffc0246257432959b8019aaef9c
horothesun <horothesun@gmail.com>
2025-04-21 16:19:30 +0100

    Keymaps improved

--- nvim/.config/nvim/lua/setup1.lua
+++ nvim/.config/nvim/lua/setup1.lua
@@ -40,8 +40,8 @@ local function on_attach(client, bufnr)
   vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, buf_opts)
 
   -- Quickfix list navigation
-  -- vim.keymap.set('n', ']q', ":cnext<CR>", buf_opts) -- default from v0.11.0
   -- vim.keymap.set('n', '[q', ":cprev<CR>", buf_opts) -- default from v0.11.0
+  -- vim.keymap.set('n', ']q', ":cnext<CR>", buf_opts) -- default from v0.11.0
   vim.keymap.set('n', '<leader>qq', ":copen<CR>", buf_opts)
   vim.keymap.set('n', '<leader>qQ', ":cclose<CR>", buf_opts)
 
@@ -101,6 +101,76 @@ vim.diagnostic.config({
   update_in_insert = false,
   severity_sort = false,
 })
+
+------------------------------------------------------------
+-- GitSigns
+------------------------------------------------------------
+require('gitsigns').setup()
+vim.keymap.set('n', '[h', ":Gitsigns prev_hunk<CR>", buf_opts)
+vim.keymap.set('n', ']h', ":Gitsigns next_hunk<CR>", buf_opts)
+vim.keymap.set('n', '<leader>hp', ":Gitsigns preview_hunk_inline<CR>", buf_opts)
+
+------------------------------------------------------------
+-- Telescope
+------------------------------------------------------------
+local telescope = require('telescope')
+telescope.setup {
+  defaults = {
+    file_sorter = require('telescope.sorters').get_fzy_sorter,
+    prompt_prefix = '> ',
+    color_devicons = true,
+    file_previewer = require('telescope.previewers').vim_buffer_cat.new,
+    grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
+    qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+    -- vimgrep_arguments = {
+    --   'rg',
+    --   '--color=never',
+    --   '--no-heading',
+    --   '--with-filename',
+    --   '--line-number',
+    --   '--column',
+    --   '--smart-case',
+    --   '--unrestricted', -- disable .gitignore handling
+    --   '--unrestricted'  -- search hidden files and directories
+    -- },
+    layout_config = { width = 0.9, height = 0.9 },
+    mappings = {
+      i = {
+        ['<esc>'] = require('telescope.actions').close,
+      }
+    },
+    file_ignore_patterns = {
+      ".git/",
+      ".idea/",
+      ".metals/",
+      ".bloop/",
+      "node_modules/"
+    }
+  },
+  pickers = {
+    find_files = { hidden = true },
+    live_grep = {
+      additional_args = function(opts)
+        return { "--hidden" }
+      end
+    }
+  },
+  extensions = {
+    fzy_native = {
+      override_generic_sorter = false,
+      override_file_sorter = true
+    }
+  }
+}
+telescope.load_extension('fzy_native')
+
+vim.keymap.set('n', '<C-p>', ":Telescope live_grep<CR>", buf_opts)
+vim.keymap.set('n', '<leader>ff', ":Telescope find_files<CR>", buf_opts)
+vim.keymap.set('n', '<leader>fb', ":Telescope buffers<CR>", buf_opts)
+vim.keymap.set('n', '<leader>fh', ":Telescope help_tags<CR>", buf_opts)
+vim.keymap.set('n', '<leader>fe', ":luafile ~/.config/nvim/pickers/emoji_picker.lua<CR>", buf_opts)
+vim.keymap.set('n', '<leader>fs', ":luafile ~/.config/nvim/pickers/symbol_picker.lua<CR>", buf_opts)
+
 
 ------------------------------------------------------------
 -- Autocompletion Setup Using nvim-cmp and LuaSnip
