a2bde001bc3515c89f2ecaf4548c77e20b2b63d8
horothesun <horothesun@gmail.com>
2025-07-05 14:17:49 +0100

    hyprlock beast Arch: gruvbox theme

--- rofi-beast-arch/.config/rofi/power_menu.sh
+++ rofi-beast-arch/.config/rofi/power_menu.sh
@@ -1,39 +1,54 @@
 #!/bin/bash
 
-POWER_MENU_OPTIONS=(lockscreen shutdown reboot logout)
+POWER_MENU_OPTIONS=(shutdown reboot suspend hibernate logout lockscreen)
+POWER_MENU_OPTIONS_LENGTH=${#POWER_MENU_OPTIONS[@]}
+
+declare -A ICONS
+ICONS[shutdown]="<span color='#cc241d'></span>"
+ICONS[reboot]="<span color='#d65d0e'>󰜉</span>"
+ICONS[suspend]="<span color='#b16286'>󰒲</span>"
+ICONS[hibernate]="<span color='#458588'>󰋊</span>"
+ICONS[logout]="<span color='#d79921'>󰍃</span>"
+ICONS[lockscreen]="<span color='#689d6a'>󰌾</span>"
 
 declare -A LABELS
-LABELS[lockscreen]="󰌾  Lock screen"
-LABELS[shutdown]="󰐥  Shutdown"
-LABELS[reboot]="󰜉  Reboot"
-LABELS[logout]="󰍃  Logout"
+LABELS[shutdown]="Shutdown"
+LABELS[reboot]="Reboot"
+LABELS[suspend]="Suspend"
+LABELS[hibernate]="Hibernate"
+LABELS[logout]="Logout"
+LABELS[lockscreen]="Lock screen"
 
 declare -A ACTIONS
-ACTIONS[lockscreen]="loginctl lock-session ${XDG_SESSION_ID-}"
 ACTIONS[shutdown]="systemctl poweroff"
 ACTIONS[reboot]="systemctl reboot"
+ACTIONS[suspend]="systemctl suspend"
+ACTIONS[hibernate]="systemctl hibernate"
 ACTIONS[logout]="hyprctl dispatch exit"
+ACTIONS[lockscreen]="loginctl lock-session ${XDG_SESSION_ID-}"
 
-SORTED_LABELS=()
-for key in "${POWER_MENU_OPTIONS[@]}"; do
-  SORTED_LABELS+=("${LABELS[$key]}")
-done
-
-JOINED_LABELS=$( IFS='|'; echo "${SORTED_LABELS[*]}" )
-
-# using `echo -n` otherwise the last item would contain a trailing newline, messing up the vertical alignment
-# ref: https://github.com/davatorium/rofi/issues/993#issuecomment-509659940
+SEP=""
 SELECTED_INDEX=$(
-  echo -n "${JOINED_LABELS}" |\
-  rofi -sep '|' -theme "${HOME}/.config/rofi/power_menu.rasi" -dmenu -i -format i -p "Power menu"
+  for key in "${POWER_MENU_OPTIONS[@]}"; do
+    printf "%s%s\0icon\x1f%s" "${SEP}" "${LABELS[${key}]}" "${ICONS[$key]}"
+    SEP="|"
+  done | \
+  rofi \
+    -sep '|' \
+    -l "${POWER_MENU_OPTIONS_LENGTH}" \
+    -theme "${HOME}/.config/rofi/power_menu.rasi" \
+    -dmenu -i -format i -p "Power menu"
 )
-SELECTED="${POWER_MENU_OPTIONS[$SELECTED_INDEX]}"
 
 if [[ -z "${SELECTED_INDEX}" ]]; then
   echo "No action selected"
-else
-  echo "SELECTED_INDEX: ${SELECTED_INDEX}"
-  echo "SELECTED: ${SELECTED}"
-  echo "ACTION: ${ACTIONS[$SELECTED]}"
-  ${ACTIONS[$SELECTED]}
+  exit 0
 fi
+
+SELECTED="${POWER_MENU_OPTIONS[$SELECTED_INDEX]}"
+
+echo "SELECTED_INDEX: ${SELECTED_INDEX}"
+echo "SELECTED: ${SELECTED}"
+echo "ACTION: ${ACTIONS[$SELECTED]}"
+
+${ACTIONS[$SELECTED]}
