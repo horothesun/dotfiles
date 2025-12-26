d485573763fca1cb9657e257d5212c531ae90bbe
horothesun <horothesun@gmail.com>
2025-12-26 00:44:16 +0000

    nvim config revamp

---
+++ nvim/.config/nvim/after/ftplugin/haskell.lua
@@ -0,0 +1,18 @@
+-- ~/.config/nvim/after/ftplugin/haskell.lua
+
+local ht  = require("haskell-tools")
+local lsp = require("config.lsp")
+
+lsp.setup()
+
+require("config.haskell_snippets").setup()
+
+local bufnr = vim.api.nvim_get_current_buf()
+local opts = { noremap = true, silent = true, buffer = bufnr }
+
+-- Hoogle signature lookup
+vim.keymap.set("n", "<leader>hs", ht.hoogle.hoogle_signature, opts)
+
+-- Evaluate all code snippets
+vim.keymap.set('n', '<space>ea', ht.lsp.buf_eval_all, opts)
+
---
+++ nvim/.config/nvim/lua/config/projectionist.lua
@@ -0,0 +1,35 @@
+-- ~/.config/nvim/lua/config/projectionist.lua
+local M = {}
+
+vim.g.projectionist_heuristics = {
+  ["stack.yaml"] = {
+    -- from src/Foo.hs -> test/FooSpec.hs
+    ["src/*.hs"] = {
+      alternate = "test/{}Spec.hs",
+      type = "source",
+    },
+    -- from test/FooSpec.hs -> src/Foo.hs
+    ["test/*Spec.hs"] = {
+      alternate = "src/{}.hs",
+      type = "test",
+    },
+  },
+
+  ["build.sbt"] = {
+    ["src/main/scala/*.scala"] = {
+      alternate = "src/test/scala/{}Spec.scala",
+      type = "source",
+    },
+    ["src/test/scala/*Spec.scala"] = {
+      alternate = "src/main/scala/{}.scala",
+      type = "test",
+    },
+  },
+}
+
+-- Keymap: <leader>gt = "go to test" (o back a source)
+vim.keymap.set("n", "<leader>gt", "<Cmd>A<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>gT", "<Cmd>AV<CR>", { noremap = true, silent = true })
+
+return M
+
