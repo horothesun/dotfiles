aed6b2c89e04b73b87d208ca675898b6defbc013
horothesun <horothesun@gmail.com>
2025-12-30 02:59:37 +0000

    LSPs cleanup

---
+++ nvim/.config/nvim/lua/config/bashls.lua
@@ -0,0 +1,36 @@
+-- ~/.config/nvim/lua/config/bashls.lua
+
+local M = {}
+
+function M.setup()
+
+  local lsp = require("config.lsp")
+
+  -- Ensure shared LSP behaviour is initialized (on_attach, <leader>ls, etc.)
+  lsp.setup()
+
+  -- TODO: implement!!! 🔥🔥🔥
+  -- require("config.bash_snippets").setup()
+
+  vim.lsp.config['bashls'] = {
+    cmd = { 'bash-language-server', 'start' },
+    settings = {
+      bashIde = {
+        -- Glob pattern for finding and parsing shell script files in the workspace.
+        -- Used by the background analysis features across files.
+
+        -- Prevent recursive scanning which will cause issues when opening a file
+        -- directly in the home directory (e.g. ~/foo.sh).
+        --
+        -- Default upstream pattern is "**/*@(.sh|.inc|.bash|.command)".
+        globPattern = vim.env.GLOB_PATTERN or '*@(.sh|.inc|.bash|.command)',
+      },
+    },
+    filetypes = { 'bash', 'sh' },
+    root_markers = { '.git' },
+  }
+
+  vim.lsp.enable('bashls')
+end
+
+return M
---
+++ nvim/.config/nvim/lua/config/ruff.lua
@@ -0,0 +1,25 @@
+-- ~/.config/nvim/lua/config/ruff.lua
+
+local M = {}
+
+function M.setup()
+
+  local lsp = require("config.lsp")
+
+  -- Ensure shared LSP behaviour is initialized (on_attach, <leader>ls, etc.)
+  lsp.setup()
+
+  -- TODO: implement!!! 🔥🔥🔥
+  -- require("config.python_snippets").setup()
+
+  vim.lsp.config['ruff'] = {
+    cmd = { 'ruff', 'server' },
+    filetypes = { 'python' },
+    root_markers = { 'pyproject.toml', 'ruff.toml', '.ruff.toml', '.git' },
+    settings = {},
+  }
+
+  vim.lsp.enable('ruff')
+end
+
+return M
