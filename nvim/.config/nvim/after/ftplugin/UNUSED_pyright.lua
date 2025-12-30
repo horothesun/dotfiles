aab077f9e6d867e6f352c4d3f6fda824a97c79f7
horothesun <horothesun@gmail.com>
2025-12-30 02:02:05 +0000

    From pyright to basedpyright

--- nvim/.config/nvim/after/ftplugin/UNUSED_pyright.lua
+++ nvim/.config/nvim/after/ftplugin/UNUSED_pyright.lua
@@ -11,12 +11,12 @@ local function set_python_path(command)
   local path = command.args
   local clients = vim.lsp.get_clients {
     bufnr = vim.api.nvim_get_current_buf(),
-    name = 'basedpyright',
+    name = 'pyright',
   }
   for _, client in ipairs(clients) do
     if client.settings then
-      ---@diagnostic disable-next-line: param-type-mismatch
-      client.settings.python = vim.tbl_deep_extend('force', client.settings.python or {}, { pythonPath = path })
+      client.settings.python =
+        vim.tbl_deep_extend('force', client.settings.python --[[@as table]], { pythonPath = path })
     else
       client.config.settings = vim.tbl_deep_extend('force', client.config.settings, { python = { pythonPath = path } })
     end
@@ -24,8 +24,8 @@ local function set_python_path(command)
   end
 end
 
-vim.lsp.config['basedpyright'] = {
-  cmd = { 'basedpyright-langserver', '--stdio' },
+vim.lsp.config['pyright'] = {
+  cmd = { 'pyright-langserver', '--stdio' },
   filetypes = { 'python' },
   root_markers = {
     'pyrightconfig.json',
@@ -37,7 +37,7 @@ vim.lsp.config['basedpyright'] = {
     '.git',
   },
   settings = {
-    basedpyright = {
+    python = {
       analysis = {
         autoSearchPaths = true,
         useLibraryCodeForTypes = true,
@@ -48,11 +48,11 @@ vim.lsp.config['basedpyright'] = {
   on_attach = function(client, bufnr)
     vim.api.nvim_buf_create_user_command(bufnr, 'LspPyrightOrganizeImports', function()
       local params = {
-        command = 'basedpyright.organizeimports',
+        command = 'pyright.organizeimports',
         arguments = { vim.uri_from_bufnr(bufnr) },
       }
 
-      -- Using client:request() directly because "basedpyright.organizeimports" is private
+      -- Using client:request() directly because "pyright.organizeimports" is private
       -- (not advertised via capabilities), which client:exec_cmd() refuses to call.
       -- https://github.com/neovim/neovim/blob/c333d64663d3b6e0dd9aa440e433d346af4a3d81/runtime/lua/vim/lsp/client.lua#L1024-L1030
       ---@diagnostic disable-next-line: param-type-mismatch
@@ -60,13 +60,12 @@ vim.lsp.config['basedpyright'] = {
     end, {
       desc = 'Organize Imports',
     })
-
     vim.api.nvim_buf_create_user_command(bufnr, 'LspPyrightSetPythonPath', set_python_path, {
-      desc = 'Reconfigure basedpyright with the provided python path',
+      desc = 'Reconfigure pyright with the provided python path',
       nargs = 1,
       complete = 'file',
     })
   end,
 }
 
-vim.lsp.enable('basedpyright')
+vim.lsp.enable('pyright')
