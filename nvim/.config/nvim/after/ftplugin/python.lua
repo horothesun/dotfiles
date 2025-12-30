bee879307f4a93e91165828a2348cc357269dec2
horothesun <horothesun@gmail.com>
2025-12-30 00:59:45 +0000

    Pyright: deprecation fix

--- nvim/.config/nvim/after/ftplugin/python.lua
+++ nvim/.config/nvim/after/ftplugin/python.lua
@@ -52,11 +52,11 @@ vim.lsp.config['pyright'] = {
         arguments = { vim.uri_from_bufnr(bufnr) },
       }
 
-      -- Using client.request() directly because "pyright.organizeimports" is private
+      -- Using client:request() directly because "pyright.organizeimports" is private
       -- (not advertised via capabilities), which client:exec_cmd() refuses to call.
       -- https://github.com/neovim/neovim/blob/c333d64663d3b6e0dd9aa440e433d346af4a3d81/runtime/lua/vim/lsp/client.lua#L1024-L1030
       ---@diagnostic disable-next-line: param-type-mismatch
-      client.request('workspace/executeCommand', params, nil, bufnr)
+      client:request('workspace/executeCommand', params, nil, bufnr)
     end, {
       desc = 'Organize Imports',
     })
