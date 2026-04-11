eba2ffe43e82336c961bf341a37efafa7978c174
horothesun <horothesun@gmail.com>
2026-04-11 23:56:29 +0100

    Waybar beast Arch: custom weather widget cleanup

--- waybar-beast-arch/bin/weather.sh
+++ waybar-beast-arch/bin/weather.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# "wttr.in?format=%c%C+|+%t+(%f)+|+%w"
 RESPONSE=$(
   curl --silent --write-out "\n%{http_code}" \
     'wttr.in?format=\{"condition_icon":"%c","condition_name":"%C","humidity":"%h","temp_actual":"%t","temp_feels":"%f","wind":"%w","moon_phase":"%m","moon_day":"%M","precipitation":"%p/3h","pressure":"%P","uv_index":"%u","sunrise":"%S","sunset":"%s"\}'
@@ -11,8 +10,30 @@ RESPONSE_BODY=$(sed '$d' <<< "${RESPONSE}")
 OUTPUT_TEXT='"🌤️ ⁉️"'
 OUTPUT_TOOLTIP='"⚠️ Service unreachable"'
 if [[ "$HTTP_CODE" == "200" ]]; then
-  OUTPUT_TEXT=$(echo "${RESPONSE_BODY}" | jq '"\(.condition_icon)\(.temp_actual | gsub("[+C]"; "")) (\(.temp_feels | gsub("[+C]"; "")))"')
-  OUTPUT_TOOLTIP=$(echo "${RESPONSE_BODY}" | jq '"Condition: \(.condition_icon)\(.condition_name)\nTemperature: \(.temp_actual | gsub("[+]"; ""))\nFeels like: \(.temp_feels | gsub("[+]"; ""))\nWind: \(.wind)\nHumidity: \(.humidity)\nPrecipitation: \(.precipitation)\nPressure: \(.pressure)\nUV index: \(.uv_index)\nSunrise: \(.sunrise)\nSunset: \(.sunset)\nMoon phase: \(.moon_phase)\nMoon day: \(.moon_day)"')
+  OUTPUT_TEXT=$(
+    echo "${RESPONSE_BODY}" |\
+      jq '
+          "\(.condition_icon)\(.temp_actual | gsub("[+C]"; "")) "
+        + "(\(.temp_feels | gsub("[+C]"; "")))"
+      '
+  )
+  OUTPUT_TOOLTIP=$(
+    echo "${RESPONSE_BODY}" |\
+      jq '
+          "Condition: \(.condition_icon)\(.condition_name)\n"
+        + "Temperature: \(.temp_actual | gsub("[+]"; ""))\n"
+        + "Feels like: \(.temp_feels | gsub("[+]"; ""))\n"
+        + "Wind: \(.wind)\n"
+        + "Humidity: \(.humidity)\n"
+        + "Precipitation: \(.precipitation)\n"
+        + "Pressure: \(.pressure)\n"
+        + "UV index: \(.uv_index)\n"
+        + "Sunrise: \(.sunrise)\n"
+        + "Sunset: \(.sunset)\n"
+        + "Moon phase: \(.moon_phase)\n"
+        + "Moon day: \(.moon_day)"
+      '
+  )
 fi
 
 echo "{
