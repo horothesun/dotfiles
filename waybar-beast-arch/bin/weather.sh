24ffd0908c8bbebd533dc039674024561cbf6aa9
horothesun <horothesun@gmail.com>
2026-04-11 23:45:05 +0100

    Waybar beast Arch: custom weather widget fix

--- waybar-beast-arch/bin/weather.sh
+++ waybar-beast-arch/bin/weather.sh
@@ -8,8 +8,8 @@ RESPONSE=$(
 HTTP_CODE=$(tail -n1 <<< "${RESPONSE}")
 RESPONSE_BODY=$(sed '$d' <<< "${RESPONSE}")
 
-OUTPUT_TEXT="❓☀️❓"
-OUTPUT_TOOLTIP="⚠️ Service unreachable"
+OUTPUT_TEXT='"🌤️ ⁉️"'
+OUTPUT_TOOLTIP='"⚠️ Service unreachable"'
 if [[ "$HTTP_CODE" == "200" ]]; then
   OUTPUT_TEXT=$(echo "${RESPONSE_BODY}" | jq '"\(.condition_icon)\(.temp_actual | gsub("[+C]"; "")) (\(.temp_feels | gsub("[+C]"; "")))"')
   OUTPUT_TOOLTIP=$(echo "${RESPONSE_BODY}" | jq '"Weather: \(.condition_icon)\(.condition_name)\nTemperature: \(.temp_actual | gsub("[+]"; ""))\nFeels like: \(.temp_feels | gsub("[+]"; ""))\nWind: \(.wind)\nHumidity: \(.humidity)\nPrecipitation: \(.precipitation)\nPressure: \(.pressure)\nUV index: \(.uv_index)\nSunrise: \(.sunrise)\nSunset: \(.sunset)\nMoon Phase: \(.moon_phase)\nMoon Day: \(.moon_day)"')
