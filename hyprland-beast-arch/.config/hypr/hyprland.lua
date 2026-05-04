9cb1dc708bf5c1f21469f7d358f903ac330c248a
horothesun <horothesun@gmail.com>
2026-05-04 16:38:28 +0100

    Cleanup

--- hyprland-beast-arch/.config/hypr/hyprland.lua
+++ hyprland-beast-arch/.config/hypr/hyprland.lua
@@ -129,60 +129,45 @@ hl.config {
 -----------------------
 
 hl.config {
-
   general    = {
     gaps_in          = 2,
     gaps_out         = 4,
-
     border_size      = 1,
-
     col              = {
       -- active_border = "rgba(306315ff)",
-      active_border = {
-        colors = { "rgba(33ccffee)", "rgba(00ff99ee)", angle = 45 }
-      },
-
+      active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)", angle = 45 } },
       -- inactive_border = "rgba(595959aa)"
       inactive_border = "rgba(ff0000aa)"
     },
-
     -- Set to true enable resizing windows by clicking and dragging on borders and gaps
     resize_on_border = false,
-
     -- Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
     allow_tearing    = false,
-
     layout           = "master"
   },
 
   decoration = {
     rounding = 4,
     rounding_power = 2,
-
     -- Change transparency of focused and unfocused windows
     active_opacity = 1.0,
     inactive_opacity = 1.0,
-
     shadow = {
       enabled = false,
       range = 4,
       render_power = 3,
       color = "rgba(1a1a1aee)",
     },
-
     blur = {
       enabled = false,
       size = 3,
       passes = 1,
-
       vibrancy = 0.1696
     }
   },
-
   animations = {
     enabled = true
   }
-
 }
 
 
