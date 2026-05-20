db02018d4e2eb91a424ccd6922dafe935ecfb0f7
horothesun <horothesun@gmail.com>
2026-05-20 17:37:01 +0100

    Waybar beast Arch: systemd-failed-units script cleanup

--- waybar-beast-arch/bin/waybar_failed_systemd_units.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-FAILED_SYSTEM_UNITS=$(systemctl --state=failed --output=json | jq --compact-output 'map(.unit)')
-FAILED_USER_UNITS=$(systemctl --user --state=failed --output=json | jq --compact-output 'map(.unit)')
-echo "{ \"failed_system_units\": ${FAILED_SYSTEM_UNITS}, \"failed_user_units\": ${FAILED_USER_UNITS} }" | jq '.'
