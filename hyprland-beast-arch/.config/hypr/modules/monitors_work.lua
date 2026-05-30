3c64f5adcfe55eaf32828eeee51241d2f582fa81
horothesun <horothesun@gmail.com>
2026-05-30 23:24:34 +0100

    Hyprland beast Arch: cleanup

--- hyprland-beast-arch/.config/hypr/modules/monitors_work.lua
+++ hyprland-beast-arch/.config/hypr/modules/monitors_work.lua
@@ -2,7 +2,7 @@ local const = require("modules.constants")
 
 hl.monitor {
   output = const.PRIMARY_MONITOR,
-  -- mode = "3840x2160@120",
+  -- 🔥🔥🔥 mode = "3840x2160@120",
   mode = "3840x2160@60",
   position = "0x0", -- impacted by scale!
   scale = "1.5",
