5d3b11a4b0c3e46f1ff396b21508110df404f38e
horothesun <horothesun@gmail.com>
2026-08-08 18:31:03 +0100

    Hyprlock beast Arch: weather info

---
+++ hyprlock-beast-arch/bin/hyprlock_weather.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+CURL_TIMEOUT_SECONDS=5
+CURL_MAXIMUM_ATTEMPTS=10
+
+OUTPUT_TEXT="🌤️ ⁉️"
+
+for ((i=1; i <= CURL_MAXIMUM_ATTEMPTS; i++)); do
+  RESPONSE=$(
+    curl --silent --fail --connect-timeout "${CURL_TIMEOUT_SECONDS}" --write-out "\n%{http_code}" \
+      'wttr.in?format=\{"condition_icon":"%c","condition_name":"%C","humidity":"%h","temp_actual":"%t","temp_feels":"%f","wind":"%w","moon_phase":"%m","moon_day":"%M","precipitation":"%p/3h","pressure":"%P","uv_index":"%u","sunrise":"%S","sunset":"%s"\}'
+  )
+
+  HTTP_CODE=$(tail -n1 <<< "${RESPONSE}")
+  RESPONSE_BODY=$(sed '$d' <<< "${RESPONSE}")
+
+  if [[ "$HTTP_CODE" == "200" ]]; then
+    OUTPUT_TEXT=$(
+      echo "${RESPONSE_BODY}" | \
+        jq --raw-output '
+            "\(.condition_icon)\(.temp_actual | gsub("[+C]"; "")) "
+          + "(\(.temp_feels | gsub("[+C]"; "")))  "
+          + "🍃 \(.wind)"
+        '
+    )
+
+    # OUTPUT_TOOLTIP=$(
+    #   echo "${RESPONSE_BODY}" | \
+    #     jq '
+    #         "\(.condition_icon)\(.condition_name)\n"
+    #       + "Temperature: \(.temp_actual | gsub("[+]"; ""))\n"
+    #       + "Feels like: \(.temp_feels | gsub("[+]"; ""))\n"
+    #       + "Wind: \(.wind)\n"
+    #       + "Humidity: \(.humidity)\n"
+    #       + "Precipitation: \(.precipitation)\n"
+    #       + "Pressure: \(.pressure)\n"
+    #       + "UV index: \(.uv_index)\n"
+    #       + "Sunrise: \(.sunrise)\n"
+    #       + "Sunset: \(.sunset)\n"
+    #       + "Moon phase: \(.moon_phase)\n"
+    #       + "Moon day: \(.moon_day)"
+    #     '
+    # )
+
+    break
+  fi
+
+  sleep "${CURL_TIMEOUT_SECONDS}"
+done
+
+echo "${OUTPUT_TEXT}"
