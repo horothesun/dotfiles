41923e74b1bddd6a5679249873ce267e77bba18b
horothesun <horothesun@gmail.com>
2026-08-21 00:47:53 +0100

    Waybar beast Arch: waybar_hwmon_temperature.sh optimized and CPU temp obtained from k10temp Tccd1 (like btop)

--- waybar-beast-arch/bin/waybar_hwmon_temperature.sh
+++ waybar-beast-arch/bin/waybar_hwmon_temperature.sh
@@ -3,7 +3,7 @@
 # find the available names/labels with the following
 # for i in /sys/class/hwmon/hwmon*/temp*_input; do
 #   echo "{\"name\":\"$(<$(dirname "$i")/name)\",\"label\":\"$(cat "${i%_*}"_label 2>/dev/null ||\
-#     echo $(basename "${i%_*}"))\", \"path\": \"$(readlink -f "$i")\"}"
+#     echo $(basename "${i%_*}"))\",\"path\":\"""$i""\"}"
 # done | jq --compact-output
 
 HWMON_NAME="$1"
@@ -11,14 +11,22 @@ HWMON_LABEL="$2"
 
 HWMON_TEMP_FILE_PATH=$(
   for i in /sys/class/hwmon/hwmon*/temp*_input; do
-    echo "{\"name\":\"$(<$(dirname "$i")/name)\",\"label\":\"$(cat "${i%_*}"_label 2>/dev/null ||\
-      echo $(basename "${i%_*}"))\", \"path\": \"$(readlink -f "$i")\"}"
-  done | jq \
-      --raw-output \
-      --arg hwmonName "${HWMON_NAME}" \
-      --arg hwmonLabel "${HWMON_LABEL}" \
-      'select(.name == $hwmonName and .label == $hwmonLabel).path'
-)
-HWMON_TEMP=$(echo "scale = 0; $(cat "${HWMON_TEMP_FILE_PATH}") / 1000" | bc)
+    dir=${i%/*}
+    [[ $(<"$dir/name") == "$HWMON_NAME" ]] || continue
 
-echo "{\"text\":${HWMON_TEMP}}"
+    label_file=${i%_*}_label
+    if [[ -r "$label_file" ]]; then
+      label=$(<"$label_file")
+    else
+      label=$(basename "${i%_*}")
+    fi
+
+    [[ "$label" == "$HWMON_LABEL" ]] && {
+      printf '%s\n' "$i"
+      break
+    }
+  done
+)
+HWMON_TEMP=$(($(< "${HWMON_TEMP_FILE_PATH}") / 1000))
+
+printf '{"text":%s}\n' "${HWMON_TEMP}"
